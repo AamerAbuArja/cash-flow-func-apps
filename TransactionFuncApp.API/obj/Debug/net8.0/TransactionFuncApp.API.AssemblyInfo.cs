86aec3e81022a8e8bc28e0893e65bd5830f80c8e
AamerAbuArja <aamerabuarja@yahoo.com>
2025-12-04 20:37:16 +0300

    Realizations Edit #9

--- TransactionFuncApp.API/obj/Debug/net8.0/TransactionFuncApp.API.AssemblyInfo.cs
+++ TransactionFuncApp.API/obj/Debug/net8.0/TransactionFuncApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransactionFuncApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6919a1666e370c675f08f667caa68296d08ac24f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d5801c29fab8f5cf8ae4bb366ea29bf31aa0a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransactionFuncApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransactionFuncApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
